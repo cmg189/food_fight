65de3e0a24320bf29e7f8335d6f70df8bb07cefd
cmg189 <tx189@yahoo.com>
2022-03-06 11:56:47 -0600

    Routing to instructions page is done. Try and change routing from inline into the backend.js file

---
+++ backend.js
@@ -0,0 +1,6 @@
+//const var main_start = document.getElementById("main_start");
+//var main_instructions = document.getElementById("main_instructions");
+
+//main_instructions.onclick = function(){
+	//location.assign('https://www.google.com/');
+//};
