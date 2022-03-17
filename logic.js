5cf95d20163653a63dc25fc1c77e4e964000208b
Chalieta Audreylia <99100112+Chalieta@users.noreply.github.com>
2022-03-17 14:04:06 -0500

    Updated setup phase
    Implement 2D and 3D foods

--- logic.js
+++ logic.js
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const foodArray = [
     {
       name: 'bread',
+      dimension: 1,
+      myLength: 3,
       directions: [
         [0, 1, 2], // horizontal
         [0, width, width*2] // vertical
@@ -15,16 +17,38 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     {
       name: 'pocky',
+      dimension: 1,
+      myLength: 3,
       directions: [
         [0, 1, 2], // horizontal
         [0, width, width*2] // vertical
       ]
     },
     {
-      name: 'bento', //TO-FIX
+      name: 'bento',
+      dimension: 2,
+      myLength: 3,
       directions: [
         [0, 1, 2, width, width+1, width+2], // horizontal
         [0, 1, width, width+1, width*2, width*2+1] // vertical
+      ]
+    },
+    {
+      name: 'donut',
+      dimension: 2,
+      myLength: 2,
+      directions: [
+        [0, 1, width, width+1], // horizontal
+        [0, 1, width, width+1] // vertical
+      ]
+    },
+    {
+      name: 'pizza',
+      dimension: 3,
+      myLength: 3,
+      directions: [
+        [0, 1, 2, width, width+1, width+2, width*2, width*2 + 1, width*2 + 2], // horizontal
+        [0, 1, 2, width, width+1, width+2, width*2, width*2 + 1, width*2 + 2] // vertical
       ]
     }
   ]
@@ -43,40 +67,98 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function generate(food) {
     let randomDirection = Math.floor(Math.random() * food.directions.length) // 0 or 1
+    let randomStart
     // console.log(randomDirection)
     let current = food.directions[randomDirection] // array containing directions
     if (randomDirection === 0) direction = 1 // horizontal
     if (randomDirection === 1) direction = 10 // vertical
-    let randomStart = Math.abs(Math.floor(Math.random() * userSquares.length - (food.directions[0].length * direction)))
+    if (food.dimension === 2)
+    {
+      if (direction === 1)
+      {
+        randomStart = Math.abs(Math.floor(Math.random() * userSquares.length - (food.myLength * direction + width)))
+      }
+      else
+      {
+        randomStart = Math.abs(Math.floor(Math.random() * userSquares.length - (food.myLength * direction + 1)))
+      }
+    }
+    else if (food.dimension === 3)
+    {
+      if (direction === 1)
+      {
+        randomStart = Math.abs(Math.floor(Math.random() * userSquares.length - (food.myLength * direction + 2*width)))
+      }
+      else
+      {
+        randomStart = Math.abs(Math.floor(Math.random() * userSquares.length - (food.myLength * direction + 2)))
+      }
+    }
+    else {
+      randomStart = Math.abs(Math.floor(Math.random() * userSquares.length - (food.myLength * direction)))
+    }
+    console.log(randomStart)
+    console.log(userSquares.length)
 
-    const isTaken = current.some(index => userSquares[randomStart + index].classList.contains('taken'))
+    const isTaken = current.some(index => userSquares[randomStart + index].classList.contains('taken')) // Debug
     const isAtRightEdge = current.some(index => (randomStart + index) % width === width - 1)
     const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0)
 
     if (!isTaken && !isAtRightEdge && !isAtLeftEdge)
     {
       current.forEach(index => userSquares[randomStart + index].classList.add('taken', food.name))
+      current.forEach(index => userSquares[randomStart + index].classList.remove('square'))
 
       if (food.name === "bread" && direction === 10)
       {
-        userSquares[randomStart].classList.add('bread-vertical')
+        userSquares[randomStart].classList.add('bread-vertical', 'square')
       }
       if (food.name === "bread" && direction === 1)
       {
-        userSquares[randomStart].classList.add('bread-horizontal')
+        userSquares[randomStart].classList.add('bread-horizontal', 'square')
       }
 
       if (food.name === "pocky" && direction === 10)
       {
-        userSquares[randomStart].classList.add('pocky-vertical')
+        userSquares[randomStart].classList.add('pocky-vertical', 'square')
       }
       if (food.name === "pocky" && direction === 1)
       {
-        userSquares[randomStart].classList.add('pocky-horizontal')
+        userSquares[randomStart].classList.add('pocky-horizontal', 'square')
+      }
+
+      if (food.name === "bento" && direction === 10)
+      {
+        userSquares[randomStart].classList.add('bento-vertical', 'square')
+      }
+      if (food.name === "bento" && direction === 1)
+      {
+        userSquares[randomStart].classList.add('bento-horizontal', 'square')
+      }
+
+      if (food.name === "donut" && direction === 10)
+      {
+        userSquares[randomStart].classList.add('donut-vertical', 'square')
+      }
+      if (food.name === "donut" && direction === 1)
+      {
+        userSquares[randomStart].classList.add('donut-horizontal', 'square')
+      }
+
+      if (food.name === "pizza" && direction === 10)
+      {
+        userSquares[randomStart].classList.add('pizza-vertical', 'square')
+      }
+      if (food.name === "pizza" && direction === 1)
+      {
+        userSquares[randomStart].classList.add('pizza-horizontal', 'square')
       }
     }
 
-    else generate(food)
+    else
+    {
+      generate(food)
+    }
   }
 
   //const bread = document.createElement('div')
@@ -85,5 +167,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   generate(foodArray[0]) // generate bread on the board
   generate(foodArray[1])
+  generate(foodArray[2])
+  generate(foodArray[3])
+  generate(foodArray[4])
 
 })
